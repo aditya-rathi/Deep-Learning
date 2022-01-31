73bb2359a10c044fc872f4de4e29f7b06448efba
Aditya Rathi <adityamr@andrew.cmu.edu>
2022-01-31 00:36:34 -0500

    83% acc achieved

--- HW 1/P2.py
+++ HW 1/P2.py
@@ -5,14 +5,65 @@ import torchvision
 import torchvision.transforms as transforms
 import torch.optim as optim
 
+train_on_gpu = torch.cuda.is_available()
+
+if not train_on_gpu:
+    print('CUDA is not available.  Training on CPU ...')
+else:
+    print('CUDA is available!  Training on GPU ...')
 
 class CNN(nn.Module):
     def __init__(self):
-        super(Net, self).__init__()
-        pass
+        super(CNN, self).__init__()
+        self.conv_layer = nn.Sequential(
+
+            # Conv Layer block 1
+            nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            # Conv Layer block 2
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Dropout2d(p=0.05),
+
+            # Conv Layer block 3
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+
+        self.fc_layer = nn.Sequential(
+            nn.Dropout(p=0.1),
+            nn.Linear(4096, 1024),
+            nn.ReLU(inplace=True),
+            nn.Linear(1024, 512),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=0.1),
+            nn.Linear(512, 10)
+        )
 
     def forward(self, x):
-        return NotImplementedError
+        # conv layers
+        x = self.conv_layer(x)
+        
+        # flatten
+        x = x.view(x.size(0), -1)
+        
+        # fc layer
+        x = self.fc_layer(x)
+
+        return x
 
 
 def main():
@@ -35,20 +86,26 @@ def main():
                'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
     # TODO: Define your optimizer and criterion.
+    criterion = nn.CrossEntropyLoss()
     model = CNN()
+    if train_on_gpu:
+        model.cuda()
+    optimizer = optim.SGD(model.parameters(), lr=2e-3 )
 
-    num_epoch = 10
+    num_epoch = 20
     for epoch in range(num_epoch):  # loop over the dataset multiple times
         running_loss = 0.0
         for i, data in enumerate(trainloader, 0):
             # get the inputs; data is a list of [inputs, labels]
             inputs, labels = data
+            if train_on_gpu:
+                inputs, labels = inputs.cuda(), labels.cuda()
 
             # zero the parameter gradients
             optimizer.zero_grad()
 
             # forward + backward + optimize
-            outputs = net(inputs)
+            outputs = model(inputs)
             loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
@@ -63,14 +120,16 @@ def main():
     print('Finished Training')
 
     PATH = './model.pth'
-    torch.save(net.state_dict(), PATH)
+    torch.save(model.state_dict(), PATH)
 
     correct = 0
     total = 0
     with torch.no_grad():
         for data in testloader:
             images, labels = data
-            outputs = net(images)
+            if train_on_gpu:
+                images, labels = images.cuda(), labels.cuda()
+            outputs = model(images)
             _, predicted = torch.max(outputs.data, 1)
             total += labels.size(0)
             correct += (predicted == labels).sum().item()

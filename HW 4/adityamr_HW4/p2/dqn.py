3ddb67e0ea85eff7319366aec95880e0d6a09c69
Aditya Rathi <adityamr@andrew.cmu.edu>
2022-04-28 12:30:01 -0400

    HW4 Completed

---
+++ HW 4/adityamr_HW4/p2/dqn.py
@@ -0,0 +1,227 @@
+import gym
+import math
+import random
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from collections import namedtuple, deque
+from itertools import count
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torchvision.transforms as T
+from PIL import Image
+from gym.wrappers.monitoring.video_recorder import VideoRecorder
+
+# The starter code follows the tutorial: https://pytorch.org/tutorials/intermediate/reinforcement_q_learning.html
+# we recommend you going through the tutorial before implement DQN algorithm
+
+
+# define environment, please don't change 
+env = gym.make('CartPole-v1')
+vid = VideoRecorder(env,'test_vid.mp4')
+
+is_ipython = 'inline' in matplotlib.get_backend()
+if is_ipython:
+    from IPython import display
+
+# define transition tuple
+Transition = namedtuple('Transition',
+                        ('state', 'action', 'next_state', 'reward'))
+
+
+class ReplayMemory(object):
+    """
+    define replay buffer class
+    """
+    def __init__(self, capacity):
+        self.memory = deque([],maxlen=capacity)
+
+    def push(self, *args):
+        """Save a transition"""
+        self.memory.append(Transition(*args))
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
+
+
+class DQN(nn.Module):
+    """
+    build your DQN model:
+    given the state, output the possiblity of actions
+    """
+    def __init__(self, in_dim, out_dim):
+        """
+        in_dim: dimension of states
+        out_dim: dimension of actions
+        """
+        super(DQN, self).__init__()
+        # build your model here
+        self.fc1 = nn.Linear(in_dim,64)
+        self.fc2 = nn.Linear(64,256)
+        self.fc3 = nn.Linear(256,64)
+        self.fc4 = nn.Linear(64,out_dim)
+
+    def forward(self, x):
+        # forward pass
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        return self.fc4(x)
+
+
+# hyper parameters you can play with
+BATCH_SIZE = 128
+GAMMA = 0.999
+EPS_START = 0.9
+EPS_END = 0.05
+EPS_DECAY = 2000
+TARGET_UPDATE = 10
+MEMORY_CAPACITY = 10000
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+n_actions = env.action_space.n
+n_states = env.observation_space.shape[0]
+
+policy_net = DQN(n_states, n_actions)
+target_net = DQN(n_states, n_actions)
+target_net.load_state_dict(policy_net.state_dict())
+target_net.eval()
+
+optimizer = optim.Adam(policy_net.parameters())
+memory = ReplayMemory(MEMORY_CAPACITY)
+
+steps_done = 0
+
+def select_action(state):
+    # given state, return the action with highest probability on the prediction of DQN model
+    # you are recommended to also implement a soft-greedy here
+    global steps_done
+    sample = random.random()
+    eps_threshold = EPS_END + (EPS_START - EPS_END) * \
+        math.exp(-1. * steps_done / EPS_DECAY)
+    steps_done += 1
+    if sample > eps_threshold:
+        with torch.no_grad():
+            return policy_net(state).max(1)[1].view(1, 1)
+    else:
+        return torch.tensor([[random.randrange(n_actions)]], device=device, dtype=torch.long)
+
+
+def optimize_model():
+    # optimize the DQN model by sampling a batch from replay buffer
+    if len(memory) < BATCH_SIZE:
+        return
+    transitions = memory.sample(BATCH_SIZE)
+
+    batch = Transition(*zip(*transitions))
+
+
+    non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
+                                          batch.next_state)), device=device, dtype=torch.bool)
+    non_final_next_states = torch.cat([s for s in batch.next_state
+                                                if s is not None])
+    state_batch = torch.cat(batch.state)
+    action_batch = torch.cat(batch.action)
+    reward_batch = torch.cat(batch.reward)
+
+    state_action_values = policy_net(state_batch).gather(1, action_batch)
+
+    next_state_values = torch.zeros(BATCH_SIZE, device=device)
+    next_state_values[non_final_mask] = target_net(non_final_next_states).max(1)[0].detach()
+    # Compute the expected Q values
+    expected_state_action_values = (next_state_values * GAMMA) + reward_batch
+
+    # Compute Huber loss
+    criterion = nn.SmoothL1Loss()
+    loss = criterion(state_action_values, expected_state_action_values.unsqueeze(1))
+
+    # Optimize the model
+    optimizer.zero_grad()
+    loss.backward()
+    for param in policy_net.parameters():
+        param.grad.data.clamp_(-1, 1)
+    optimizer.step()
+
+
+num_episodes = 100
+episode_durations = []
+for i_episode in range(num_episodes):
+    # Initialize the environment and state
+    state = env.reset()
+    state = torch.from_numpy(state).float().view(1, -1)
+    for t in count():
+        # Select and perform an action
+        action = select_action(state)
+        new_state, reward, done, _ = env.step(action.item())
+        reward = torch.tensor([reward])
+
+        # # Observe new state
+        if not done:
+            next_state = torch.from_numpy(new_state).float().view(1, -1)
+        else:
+            next_state = None
+
+        # Store the transition in memory
+        memory.push(state, action, next_state, reward)
+
+        # Move to the next state
+        state = next_state
+
+        # Perform one step of the optimization (on the target network)
+        optimize_model()
+        if done:
+            episode_durations.append(t + 1)
+            print("Episode: {}, duration: {}".format(i_episode, t+1))
+            break
+    
+    # Update the target network, copying all weights and biases in DQN
+    if i_episode % TARGET_UPDATE == 0:
+        target_net.load_state_dict(policy_net.state_dict())
+
+
+# plot time duration
+plt.figure()
+plt.plot(np.arange(len(episode_durations)), episode_durations)
+plt.show()
+
+#save model
+torch.save({
+    'model_state_dict':policy_net.state_dict(),
+    'optimizer_state_dict':optimizer.state_dict()
+},'p2_model.ckpt')
+
+
+# visualize 
+for i in range(10):
+    state = env.reset()
+    state = torch.from_numpy(state).float().view(1, -1)
+    for t in count():
+        env.render()
+        vid.capture_frame()
+        # Select and perform an action
+        action = select_action(state)
+        new_state, reward, done, _ = env.step(action.item())
+        reward = torch.tensor([reward])
+
+        # Observe new state
+        if not done:
+            next_state = torch.from_numpy(new_state).float().view(1, -1)
+        else:
+            next_state = None
+
+        # Move to the next state
+        state = next_state
+
+        if done:
+            episode_durations.append(t + 1)
+            print("Duration:", t+1)
+            break
+
+env.close()
+vid.close()
